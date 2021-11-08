0daf842f0502777053ecef7b71523a774256085b
alekss.dakulis <alekss.dakulis@visma.com>
2021-11-08 16:34:53 +0200

    Add services

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -9,22 +9,26 @@ import { HEROES } from '../mock-heroes';
 })
 
 export class HeroesComponent implements OnInit {
-  /*hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };*/
+
   selectedHero?: Hero;
 
-  heroes = HEROES;
+  heroes: Hero[] = [];
 
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
 
-  constructor() { }
+  constructor(private heroService: HeroService) {
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
+  }
 
   ngOnInit(): void {
+    this.getHeroes();
   }
 
 }
